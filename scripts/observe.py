10337fbc2e4cbe882243ae748fdc4708e483ed4a
michaelgailling <michael.gailling@gmail.com>
2018-10-02 21:22:17 -0400

    Added simple gui

--- scripts/observe.py
+++ scripts/observe.py
@@ -51,11 +51,77 @@ class ParseContent(HTMLParser):
     def handle_pi(self, data):
         pass
 
-class HtmlScrape(request):
-    def __init__(self, parent):
-        url = ""
-        hdr = {'User-Agent': 'metaprinter'}
-        req = self.Request(url, headers=hdr)
-        html = self.urlopen(req).read()
 
-        html = html.decode("utf-8")
+class HtmlScrape:
+    def __init__(self, parent=None, url="", header={'User-Agent': 'metaprinter'}):
+        parser = ParseContent()
+        self.url = url
+        self.header = header
+        req = request.Request(self.url, headers=self.header)
+        html = request.urlopen(req).read()
+
+        self.html = html.decode("utf-8")
+
+
+class InfoView(QFrame):
+    def __init__(self, parent=None):
+        super(InfoView, self).__init__(parent)
+        scraper = HtmlScrape(parent=self, url="")
+        print(scraper.html)
+
+
+class IVTabs(QTabWidget):
+    def __init__(self, parent=None):
+        super(IVTabs, self).__init__(parent)
+
+    def new_tab(self):
+        self.addTab(InfoView(), "Tab " + str(self.count() + 1))
+        self.setCurrentIndex(self.count() - 1)
+
+    def close_tab(self):
+        self.removeTab(self.currentIndex())
+
+
+class AppWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(AppWindow, self).__init__(parent)
+        self.title = 'obac'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 400
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.IVT = IVTabs(parent=self)
+        self.setCentralWidget(self.IVT)
+        self.initUI()
+
+    def initUI(self):
+        main_menu = self.menuBar()
+
+        file_menu = main_menu.addMenu('File')
+
+        new_tab_button = QAction(QIcon('exit24.png'), 'New Tab', self)
+        new_tab_button.setShortcut('Ctrl+T')
+        new_tab_button.triggered.connect(self.IVT.new_tab)
+        file_menu.addAction(new_tab_button)
+
+        close_tab_button = QAction(QIcon('exit24.png'), 'Close Tab', self)
+        close_tab_button.setShortcut('Ctrl+Alt+T')
+        close_tab_button.triggered.connect(self.IVT.close_tab)
+        file_menu.addAction(close_tab_button)
+
+        exit_button = QAction(QIcon('exit24.png'), 'Exit', self)
+        exit_button.setShortcut('Ctrl+Q')
+        exit_button.setStatusTip('Exit application')
+        exit_button.triggered.connect(self.close)
+        file_menu.addAction(exit_button)
+
+        self.IVT.new_tab()
+        self.show()
+
+if __name__ == "__main__":
+    qApp = QApplication(sys.argv)
+    aw = AppWindow()
+    aw.show()
+    sys.exit(qApp.exec_())
